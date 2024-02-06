63d9ea7dc53ed7a70bf143b19a2895310258fb50
riven <fengdateng@163.com>
2024-02-06 20:23:09 +0800

    [add] Add agent self_ask_with_search_agent.py file.

---
+++ LangChain/agent/self_ask_with_search_agent.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+
+from langchain import hub
+
+from langchain_community.utilities import SerpAPIWrapper
+
+from langchain.agents import Tool, AgentExecutor, create_self_ask_with_search_agent
+
+from langchain_openai import ChatOpenAI
+
+import langchain
+
+langchain.debug = True
+
+# Initialize environment variables from .env file
+load_dotenv(verbose=True)
+
+# Model
+llm = ChatOpenAI(model_name="gpt-4")
+
+# Tools
+search_api_wrapper = SerpAPIWrapper()
+tools = [
+    Tool(
+        name="Intermediate Answer",
+        func=search_api_wrapper.run,
+        description="useful for when you need to ask with search"
+    )
+]
+
+# Chat prompt template
+prompt_template = hub.pull("hwchase17/self-ask-with-search")
+
+# Create Structured Chat Agent
+agent = create_self_ask_with_search_agent(llm=llm, tools=tools, prompt=prompt_template)
+
+# Run Agent
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+agent_executor.invoke({"input": "龙珠动漫里的主角的夫人分别叫什么名字？"})
