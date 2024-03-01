61dd408389ccb61d3aea228a99cc783152c2b9fa
riven <fengdateng@163.com>
2024-03-01 10:33:14 +0800

    [add] Add retrieval embedding_models.py.

---
+++ LangChain/retrieval/embedding_models.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAIEmbeddings
+
+# Initialize environment variables from .env file
+load_dotenv(verbose=True)
+
+"""
+    CASE 1:
+        OpenAIEmbeddings.
+"""
+
+# Model
+embeddings_model = OpenAIEmbeddings()
+
+# Documents
+embedded_docs = embeddings_model.embed_documents(
+    [
+        "Hi there!",
+        "Oh, hello!",
+        "What's your name?",
+        "My friends call me World",
+        "Hello World!"
+    ]
+)
+
+# Query
+embedded_query = embeddings_model.embed_query("What was the name mentioned in the conversation?")
