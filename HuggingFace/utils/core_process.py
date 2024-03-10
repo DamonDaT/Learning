d39aea1c5e0ad6f132b3381fae0ede51fb72f697
dateng <fengdateng@163.com>
2024-03-10 21:38:08 +0800

    [add] Add dataset process.

--- HuggingFace/utils/core_process.py
+++ HuggingFace/utils/core_process.py
@@ -1,18 +1,15 @@
 import os
 
 from dotenv import load_dotenv
+
 from huggingface_hub import snapshot_download
+from datasets import load_dataset
 
 # Initialize environment variables from .env file
 load_dotenv(verbose=True)
 
 """
-    Args:
-        repo_id: A user or an organization name and a repo name separated by a `/`.
-        local_dir: If provided, the downloaded files will be placed under this directory.
-        local_dir_use_symlinks: To be used with `local_dir`.
-        ignore_patterns: If provided, files matching any of the patterns are not downloaded.
-        token: A token to be used for the download.
+    1. Download model.
 """
 
 snapshot_download(
@@ -22,3 +19,9 @@ snapshot_download(
     ignore_patterns=["*.bin"],
     token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
 )
+
+"""
+    2. Load dataset.
+"""
+
+dataset = load_dataset(r"/home/dateng/dataset/huggingface/yelp_review_full")
